d135f04d3070ebf35e0039d9c35b634682f71192
Code-By-Adarsh <adarshmishra70931@gmail.com>
2025-10-18 23:36:09 +0530

    Add: ExtentReportManager java file

---
+++ Project_3_EcomAutomationFramework/src/main/java/Report/ExtentReportManager.java
@@ -0,0 +1,27 @@
+package Report;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentReportManager {
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            String reportPath = System.getProperty("user.dir") + "\\reports\\SwagLabsTestReport.html";
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+
+            spark.config().setDocumentTitle("Test Report");
+            spark.config().setReportName("Automation Results");
+            spark.config().setTheme(Theme.DARK);
+
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+            extent.setSystemInfo("Automation Tester: ", "Adarsh Mishra");
+            extent.setSystemInfo("Browser: ", "Chrome");
+            extent.setSystemInfo("Report: ","Swag Labs Test");
+        }
+       return extent;
+    }
+}
