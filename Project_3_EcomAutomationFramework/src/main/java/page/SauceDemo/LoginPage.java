ffd69e10fe400a2d0a990d8512ca4f77190feba0
Code-By-Adarsh <adarshmishra70931@gmail.com>
2025-10-18 23:34:22 +0530

    Add: LoginPage java file POM

---
+++ Project_3_EcomAutomationFramework/src/main/java/page/SauceDemo/LoginPage.java
@@ -0,0 +1,33 @@
+package page.SauceDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    //locator
+    private By usernameTextBox = By.id("user-name");
+    private By passwordTextBox = By.id("password");
+    private By loginButton = By.id("login-button");
+
+    //constructor
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    //action
+    public void enterUsername(String username){
+        driver.findElement(usernameTextBox).clear();
+        driver.findElement(usernameTextBox).sendKeys(username);
+    }
+
+    public void enterPassword(String password){
+        driver.findElement(passwordTextBox).clear();
+        driver.findElement(passwordTextBox).sendKeys(password);
+    }
+
+    public void clickLogin(){
+        driver.findElement(loginButton).click();
+    }
+}
